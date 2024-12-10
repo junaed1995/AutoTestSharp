bd8a582147342fe49a4c31164b23b7de05f260fe
Junaed <junaedsiddiquee@gmail.com>
2024-12-10 21:17:28 +0530

    Added code for handling alerts,dynamic dropdowns and window handles.

---
+++ NUnitProj/AlertsAndAutoSuggstions.cs
@@ -0,0 +1,117 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Interactions;
+
+namespace NUnitProj
+{
+    internal class AlertsAndAutoSuggstions
+    {
+#pragma warning disable NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+        IWebDriver driver;
+#pragma warning restore NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+        [SetUp]
+
+        public void StartBrowser()
+
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/AutomationPractice/";
+
+
+        }
+
+        [Test]
+        public void test_AutoSuggestiveDropDowns()
+
+        {
+
+            driver.FindElement(By.Id("autocomplete")).SendKeys("ind");
+            Thread.Sleep(3000);
+
+            IList<IWebElement> options = driver.FindElements(By.CssSelector(".ui-menu-item div"));
+
+            foreach (IWebElement option in options)
+            {
+                if (option.Text.Equals("India"))
+                {
+                    option.Click();
+                }
+
+            }
+
+            TestContext.Progress.WriteLine(driver.FindElement(By.Id("autocomplete")).GetDomAttribute("value"));
+
+        }
+
+        [Test]
+        public void test_Alert()
+
+        {
+            String name = "Rahul";
+            driver.FindElement(By.CssSelector("#name")).SendKeys(name);
+            driver.FindElement(By.CssSelector("input[onclick*='displayConfirm']")).Click();
+            IAlert alert = driver.SwitchTo().Alert();
+            String alertText = alert.Text;
+            alert.Accept();
+            //   driver.SwitchTo().Alert().Dismiss();
+            //  driver.SwitchTo().Alert().SendKeys("hello");
+
+            StringAssert.Contains(name, alertText);
+
+        }
+
+        [Test]
+        public void test_Actions()
+
+        {
+
+            driver.Url = "https://rahulshettyacademy.com";
+            Actions a = new Actions(driver);
+            a.MoveToElement(driver.FindElement(By.CssSelector("a.dropdown-toggle"))).Perform();
+            a.MoveToElement(driver.FindElement(By.XPath("//ul[@class='dropdown-menu']/li[1]/a"))).Click().Perform();
+
+            driver.Url = "https://demoqa.com/droppable/";
+            a.DragAndDrop(driver.FindElement(By.Id("draggable")), driver.FindElement(By.Id("droppable"))).Perform();
+        }
+
+
+        [Test]
+
+        public void Frames()
+
+        {
+            //scroll
+            IWebElement frameScroll = driver.FindElement(By.Id("courses-iframe"));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", frameScroll);
+
+            // id, name, index
+            driver.SwitchTo().Frame("courses-iframe");
+            driver.FindElement(By.LinkText("All Access Plan")).Click();
+            TestContext.Progress.WriteLine(driver.FindElement(By.CssSelector("h1")).Text);
+            driver.SwitchTo().DefaultContent();
+            
+            TestContext.Progress.WriteLine(driver.FindElement(By.CssSelector("h1")).Text);
+
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            Thread.Sleep(2000);
+            driver.Quit();
+        }
+
+    }
+}
---
+++ NUnitProj/WindowHandles.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace NUnitProj
+{
+    internal class WindowHandles
+    {
+#pragma warning disable NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+        IWebDriver driver;
+#pragma warning restore NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+
+        [SetUp]
+
+        public void StartBrowser()
+
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
+
+        }
+        [Test]
+        public void WindowHandle()
+
+        {
+            String email = "mentor@rahulshettyacademy.com";
+            String parentWindowId = driver.CurrentWindowHandle;
+            driver.FindElement(By.ClassName("blinkingText")).Click();
+            
+            //Assert.AreEqual(2, driver.WindowHandles.Count);//1
+            Assert.That(2,Is.EqualTo(driver.WindowHandles.Count));
+
+            //String childWindowName = driver.WindowHandles[1];
+
+            IList<string> handles = driver.WindowHandles;
+
+            driver.SwitchTo().Window(handles[1]);
+
+            TestContext.Progress.WriteLine(driver.FindElement(By.CssSelector(".red")).Text);
+            String text = driver.FindElement(By.CssSelector(".red")).Text;
+
+            // Please email us at mentor @rahulshettyacademy.com with below template to receive response
+
+            String[] splittedText = text.Split("at");
+
+            String[] trimmedString = splittedText[1].Trim().Split(" ");
+
+            //Assert.AreEqual(email, trimmedString[0]);
+            Assert.That(email, Is.EqualTo(trimmedString[0]));
+            driver.SwitchTo().Window(parentWindowId);
+
+            driver.FindElement(By.Id("username")).SendKeys(trimmedString[0]);
+
+        }
+
+
+
+    }
+}
